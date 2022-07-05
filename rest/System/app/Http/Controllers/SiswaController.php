049d0ac96dcae2c3ad2751343b4f85b5b915b37e
elseptiawan <septiawanelshandi@gmail.com>
2022-07-05 09:16:37 +0700

    Fixing Get Siswa

--- rest/System/app/Http/Controllers/SiswaController.php
+++ rest/System/app/Http/Controllers/SiswaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use App\Models\Siswa;
 
 class SiswaController extends Controller
